55b9b818543993003d9628ad31cac3279064672b
c-c-cole <coe18@pitt.edu>
2025-09-20 16:11:47 -0400

    page box layout

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # SteelHacks-XII
 
-LET'S GOOOOOOOOOOOOOOOOOOOOOOO 
+To run front end
+    enter client
+    npm install
+    npm run dev
+
+To fix client error
+    rm -rf node_modules
+    rm package-lock.json
+    npm cache clean -force
+    npm install
--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -17,20 +17,37 @@ function App() {
 
   return (
     <>
-    <div class = "title">
-      <h2>Access Map</h2>
+    <div class = "app-container">
+    <div class = "home-bar">
+      <div class = "about">
+        <h1>Access Map</h1>
+      </div>
+      <div class = "extra-info">
+        <p>Sign Up</p>
+        <p>Login</p>
+      </div>
     </div>
-      <div class = "login">
+
+
+    <div class = "container">
+      <div class = "map">
+        <h2>Select a location to view report</h2>
+      </div>
+    </div>
+    <div>
+    <div class = "comment">
         <input
         type = "text"
         value = {name}
         onChange={handleInput}
-        placeholder = "Enter Username">
+        placeholder = "Enter text">
         </input>
         <button onClick = {handleClick}>
           Submit
         </button>
       </div>
+    </div>
+    </div>
     </>
   )
 }
